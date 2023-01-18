ae48d3c3953127a48ee1bf8d4be38aac8110b4a3
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2023-01-18 10:19:06 -0400

    Initial commit

---
+++ HealthSystem/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem
+{
+    internal class Player
+    {
+        private string name;
+        private int health;
+        private int lives;
+
+
+        public Player(string name = "Bobert")
+        {
+            this.name = name;
+            health = 100;
+            lives = 3;
+        }
+
+        public void TakeDamage(int hp)
+        {
+            health -= hp;
+            if (health <= 0) health = 0;
+        }
+
+        public void Heal(int hp)
+        {
+            health += hp;
+            if (health > 100) health = 100;
+        }
+
+        public void ShowHUD()
+        {
+            Console.WriteLine("Player: " + name);
+            Console.WriteLine(name + "'s Health = " + health);
+            Console.WriteLine(name + "'s lives = " + lives);
+        }
+    }
+}
---
+++ HealthSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Phil");
+            Console.WriteLine("Health System\n");
+            player.ShowHUD();
+            Console.ReadKey(true);
+            Console.WriteLine("");
+            player.TakeDamage(34);
+            player.ShowHUD();
+            Console.ReadKey(true);
+            Console.WriteLine("");
+            player.Heal(4);
+            player.ShowHUD();
+            Console.ReadKey(true);
+        }
+    }
+}
